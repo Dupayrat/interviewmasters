ffccd726862b1e79950db84ee933743247945b68
Philippe Dupayrat <philippedupayrat@gmail.com>
2020-06-02 15:07:46 +0200

    getting first intent of form correct

--- app/controllers/interview_preparations_controller.rb
+++ app/controllers/interview_preparations_controller.rb
@@ -1,13 +1,25 @@
 class InterviewPreparationsController < ApplicationController
-def index
-end
+  def index
+  end
 
-def new
+  def new
+  @interview_preparation = InterviewPreparation.new
+  end
 
-end
+  def create
+    @interview_preparation = InterviewPreparation.new(interview_preparation_params)
+    @interview_preparation.user = current_user
+    if @interview_preparation.save!
+      redirect_to interview_preparation_path(@interview_preparation)
+    else
+      render :new
+    end
+  end
 
-def create
+  private
 
-end
+  def interview_preparation_params
+    params.require(:interview_preparation).permit(:company, :job, :interview_date, :experience_expectation)
+  end
 
 end
