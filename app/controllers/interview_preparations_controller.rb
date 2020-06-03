7e837a61799c91d73e743f584a50531f731e6008
Anthony Christen <anthony1998@bluewin.ch>
2020-06-03 18:20:08 +0200

    collapse added to display infos in the display zone

--- app/controllers/interview_preparations_controller.rb
+++ app/controllers/interview_preparations_controller.rb
@@ -15,16 +15,16 @@ class InterviewPreparationsController < ApplicationController
       "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20Interview&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
       "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20other&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2"
     ]
-      urls.each do |url|
-        json = JSON.parse(open(url).read)
-        @company_videos << json["items"].map do |video|
-          {
-          title: video["snippet"]["title"],
-          thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
-          url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
-           }
-        end
-      end
+      # urls.each do |url|
+      #   json = JSON.parse(open(url).read)
+      #   @company_videos << json["items"].map do |video|
+      #     {
+      #     title: video["snippet"]["title"],
+      #     thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
+      #     url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
+      #      }
+      #   end
+      # end
   end
 
   def new
