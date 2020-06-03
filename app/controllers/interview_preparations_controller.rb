dca66d53bbda85c6cd2cf2226247b8a2617056a7
Anthony Christen <anthony1998@bluewin.ch>
2020-06-03 15:46:11 +0200

    initialization save

--- app/controllers/interview_preparations_controller.rb
+++ app/controllers/interview_preparations_controller.rb
@@ -6,26 +6,26 @@ class InterviewPreparationsController < ApplicationController
     @interview_preparations = current_user.interview_preparations
   end
 
-    def show
-      @interview_preparation = InterviewPreparation.find(params[:id])
+  def show
+    @interview_preparation = InterviewPreparation.find(params[:id])
 
-      @company_videos = []
-      urls = [
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20ceo&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20Interview&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20other&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2"
-      ]
-        urls.each do |url|
-          json = JSON.parse(open(url).read)
-          @company_videos << json["items"].map do |video|
-            {
-            title: video["snippet"]["title"],
-            thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
-            url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
-             }
-          end
+    @company_videos = []
+    urls = [
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20ceo&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20Interview&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20other&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2"
+    ]
+      urls.each do |url|
+        json = JSON.parse(open(url).read)
+        @company_videos << json["items"].map do |video|
+          {
+          title: video["snippet"]["title"],
+          thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
+          url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
+           }
         end
-    end
+      end
+  end
 
   def new
     @interview_preparation = InterviewPreparation.new
