b8304fd87169e38a0aa53df4fca5a4688213cf24
Anthony Christen <anthony1998@bluewin.ch>
2020-06-04 11:03:59 +0200

    problem with the API

--- app/controllers/interview_preparations_controller.rb
+++ app/controllers/interview_preparations_controller.rb
@@ -9,22 +9,22 @@ class InterviewPreparationsController < ApplicationController
   def show
     @interview_preparation = InterviewPreparation.find(params[:id])
 
-    @company_videos = []
-    urls = [
-      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20ceo&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
-      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20Interview&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
-      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20start%20up&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2"
-    ]
-      urls.each do |url|
-        json = JSON.parse(open(url).read)
-        @company_videos << json["items"].map do |video|
-          {
-          title: video["snippet"]["title"],
-          thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
-          url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
-           }
-        end
-      end
+    # @company_videos = []
+    # urls = [
+    #   "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20ceo&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
+    #   "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20Interview&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
+    #   "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20start%20up&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2"
+    # ]
+    #   urls.each do |url|
+    #     json = JSON.parse(open(url).read)
+    #     @company_videos << json["items"].map do |video|
+    #       {
+    #       title: video["snippet"]["title"],
+    #       thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
+    #       url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
+    #        }
+    #     end
+    #   end
   end
 
   def new
