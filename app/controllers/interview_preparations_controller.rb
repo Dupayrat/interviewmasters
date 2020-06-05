403bded9e869b31afd8b756e987ab77cf8b39f83
Anthony Christen <anthony1998@bluewin.ch>
2020-06-05 11:31:06 +0200

    validations added for InterviewPreparation

--- app/controllers/interview_preparations_controller.rb
+++ app/controllers/interview_preparations_controller.rb
@@ -53,15 +53,15 @@ class InterviewPreparationsController < ApplicationController
     # FREQUENTLY ASKED QUESTIONS (COMPANY)
     # ------------------------------------
 
-    @company_questions = []
+    # @company_questions = []
 
-    company = @interview_preparation.company.downcase.gsub(/\s/, '-')
+    # company = @interview_preparation.company.downcase.gsub(/\s/, '-')
 
-    doc = Nokogiri::HTML(URI.open("https://fr.glassdoor.ch/Entretien/manor-questions-entretien-d-embauche-SRCH_KE0,5.htm"))
+    # doc = Nokogiri::HTML(URI.open("https://fr.glassdoor.ch/Entretien/manor-questions-entretien-d-embauche-SRCH_KE0,5.htm"))
 
-    doc.search('.questionText').each do |element|
-      @company_questions << element.text
-    end
+    # doc.search('.questionText').each do |element|
+    #   @company_questions << element.text
+    # end
   end
 
   def new
@@ -76,7 +76,7 @@ class InterviewPreparationsController < ApplicationController
     @interview_preparation = InterviewPreparation.new(interview_preparation_params)
     @interview_preparation.user = current_user
 
-    if @interview_preparation.save!
+    if @interview_preparation.save
       redirect_to interview_preparation_path(@interview_preparation)
     else
       render :new
@@ -91,7 +91,7 @@ class InterviewPreparationsController < ApplicationController
     :job,
     :interview_date,
     :experience_expectation,
-    missions_attributes: [:id, :name]
+    missions_attributes: [:id, :name, :label]
     )
   end
 end
