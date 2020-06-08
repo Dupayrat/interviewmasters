bdc11fd717c0dda71002307c544cc0bf2417531b
john john de weerdt <johnjohn20015@hotmail.com>
2020-06-08 10:31:51 +0200

    bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_151147) do
+ActiveRecord::Schema.define(version: 2020_06_08_082726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "interview_date"
+    t.integer "price_per_hour"
+    t.integer "duration"
+    t.bigint "interview_preparation_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["interview_preparation_id"], name: "index_bookings_on_interview_preparation_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "coaches", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "expertise"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_coaches_on_user_id"
+  end
 
   create_table "interview_preparations", force: :cascade do |t|
     t.string "company"
@@ -49,10 +69,14 @@ ActiveRecord::Schema.define(version: 2020_06_02_151147) do
     t.string "industry"
     t.string "image"
     t.string "description"
+    t.string "expertise"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "interview_preparations"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "coaches", "users"
   add_foreign_key "interview_preparations", "users"
   add_foreign_key "missions", "interview_preparations"
 end
