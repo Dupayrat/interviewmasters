01c4caf130cfb0a50c5f892a57d247a2d5459fbc
Dupayrat <57572754+Dupayrat@users.noreply.github.com>
2020-06-10 09:58:51 +0200

    Update active_challenge_card.js

--- app/javascript/packs/active_challenge_card.js
+++ app/javascript/packs/active_challenge_card.js
@@ -4,8 +4,8 @@ const challenge_card_color = () => {
   challenges_cards.forEach((challenge_card) => {
     challenge_card.addEventListener('click', (event) => {
       const actives_challenges_cards = document.querySelectorAll(".active-challenge-card");
-      actives_challenges_cards.forEach((active_hallenge_card) => {
-        active_hallenge_card.classList.remove("active-challenge-card");
+      actives_challenges_cards.forEach((active_challenge_card) => {
+        active_challenge_card.classList.remove("active-challenge-card");
       });
 
       challenge_card.classList.toggle("active-challenge-card");
