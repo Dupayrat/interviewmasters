8f33e8faabfc48b1af17c453afc62f421b556f8b
Anthony Christen <anthony1998@bluewin.ch>
2020-06-11 10:00:21 +0200

    progress bar done

--- app/javascript/packs/progress_bar.js
+++ app/javascript/packs/progress_bar.js
@@ -1,13 +1,13 @@
 const progress_bar = () => {
   const progress_bars = document.querySelectorAll(".progress-bar")
   progress_bars.forEach((progress_bar) => {
-    progression = progress_bar.innerText.replace("%", "")
+    const progression = Number.parseInt(progress_bar.innerText.replace(/%/, ''), 10);
     if (progression < 50) {
-      progress_bar.setAttribute("style", "background-color: red;")
+      progress_bar.classList.add("progress-bar-red")
+    } else if (progression === 100) {
+      progress_bar.classList.add("progress-bar-green")
     } else if (progression >= 50) {
-      progress_bar.setAttribute("style", "background-color: $orange;")
-    } else if (progression = 100) {
-      progress_bar.setAttribute("style", "background-color: #3ac162;")
+      progress_bar.classList.add("progress-bar-yellow")
     }
   });
 };
