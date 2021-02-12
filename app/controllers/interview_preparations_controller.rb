38bd0b377319517ce026f15ae0148f9e30ebd6fe
Philippe du Payrat <57572754+Dupayrat@users.noreply.github.com>
2021-02-12 23:03:54 +0100

    Update interview_preparations_controller.rb

--- app/controllers/interview_preparations_controller.rb
+++ app/controllers/interview_preparations_controller.rb
@@ -40,21 +40,21 @@ before_action :set_interview_preparation, only: [:show, :edit, :update, :scrap_a
 
     @company_videos = []
 
-    urls = [
+    #urls = [
       "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20ceo&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
       "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20Interview&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2",
       "https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{@interview_preparation.company}%20start%20up&type=video&relevanceLanguage=FR&key=#{ENV.fetch('YOUTUBE_API_KEY')}&maxResults=2"
     ]
-      urls.each do |url|
-        json = JSON.parse(open(url).read)
-        @company_videos << json["items"].map do |video|
-          {
-          title: video["snippet"]["title"],
-          thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
-          url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
-           }
-        end
-      end
+      # urls.each do |url|
+      #  'json = JSON.parse(open(url).read)
+      #  @company_videos << json["items"].map do |video|
+      #    {
+      #    title: video["snippet"]["title"],
+      #    thumbnails: video["snippet"]["thumbnails"]["default"]["url"],
+       #   url: "https://www.youtube.com/watch?v=#{video["id"]["videoId"]}"
+       #    }
+       # end
+      # end
 
     # ------------------
     # CANDIDATE PREPARATION (COMPANY QUESTIONS)
